b5db4521d386119adcb7b11afd0bf244a92919fd
Feraspar <97383817+Feraspar@users.noreply.github.com>
2024-08-21 19:11:53 +0700

    Создание второй страницы и реализация навигации

--- Sounds_UWP/ViewModel/MainPageViewModel.cs
+++ Sounds_UWP/ViewModel/MainPageViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Sounds_UWP.Model;
+using Sounds_UWP.Services;
+using Sounds_UWP.View;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -7,6 +10,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Windows.Storage;
 
 namespace Sounds_UWP.ViewModel
@@ -14,17 +18,32 @@ namespace Sounds_UWP.ViewModel
     public class MainPageViewModel : ObservableObject
     {
         public ObservableCollection<SoundModel> Sounds { get; private set; }
+        public SoundModel SelectedSound
+        {
+            get => _selectedSound;
+            set
+            {
+                SetProperty(ref _selectedSound, value);
+                if (value != null)
+                {
+                    NavigateToSoundCommand.Execute(value);
+                }
+            }
+        }
+        public ICommand NavigateToSoundCommand { get; private set; }
 
         private StorageFolder _localFolder;
+        private SoundModel _selectedSound;
 
         public MainPageViewModel()
         {
             Sounds = new ObservableCollection<SoundModel>();
+            NavigateToSoundCommand = new RelayCommand<SoundModel>(NavigateToSound);
             _localFolder = ApplicationData.Current.LocalFolder;
             _ = InitializeSounds();
         }
 
-        public async Task InitializeSounds()
+        private async Task InitializeSounds()
         {
             List<string> images = new List<string>()
             {
@@ -34,7 +53,16 @@ namespace Sounds_UWP.ViewModel
                 "5539177-middle.png",
                 "8400_1_1703597400.jpeg",
                 "IBM_Simon_Personal_Communicator.png",
-                "IMG_0575.jpg"
+                "IMG_0575.jpg",
+                "expo-go-app.png",
+                "443_10_1696353179.jpg",
+                "123E231.png",
+                "5539177-middle.png",
+                "8400_1_1703597400.jpeg",
+                "IBM_Simon_Personal_Communicator.png",
+                "IMG_0575.jpg",
+                "5539177-middle.png",
+                "vliublennye-svidanie-siluety-paren-i-devushka-bereg-more-zve.jpg"
             };
 
             foreach (var imageName in images)
@@ -56,6 +84,12 @@ namespace Sounds_UWP.ViewModel
 
                 Sounds.Add(new SoundModel { Name = $"Звук {i + 1}", BackgroundUri = imageFile.Path });
             }
+        }
+
+        private void NavigateToSound(SoundModel sound)
+        {
+            NavigationService navigationService = new NavigationService();
+            navigationService.Navigate(typeof(SoundDetailPage), sound);
         }
     }
 }
---
+++ Sounds_UWP/ViewModel/SoundDetailPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Sounds_UWP.Model;
+using Sounds_UWP.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Sounds_UWP.ViewModel
+{
+    public class SoundDetailPageViewModel : ObservableObject
+    {
+        public SoundModel SelectedSound { get; set; }
+        public ICommand NavigateToBackCommand { get; private set; }
+
+        public SoundDetailPageViewModel(SoundModel selectedSound)
+        {
+            SelectedSound = selectedSound;
+            NavigateToBackCommand = new RelayCommand(NavigateToBack);
+        }
+
+        private void NavigateToBack()
+        {
+            NavigationService navigationService = new NavigationService();
+            navigationService.NavigateToBack();
+        }
+    }
+}
